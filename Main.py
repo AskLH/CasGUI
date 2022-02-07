7524eb537eb3eaffece7f2bd49e6c319935ac729
Markuliaso <markus.utoft@mail.com>
2022-02-07 11:26:16 +0100

    Koden fra sidste GUI projekt til at gå ud fra

--- Main.py
+++ Main.py
@@ -1,7 +1,52 @@
 from sympy import *
 import tkinter
 
-class mainwindow:
+    class mainWindowClass:
+        def __init__(self):
+            filename = 'betalinger.pk'
+            self.fodboldtur = {}
+            infile = open(filename, 'rb')
+            self.fodboldtur = pickle.load(infile)
+            infile.close()
+            # self.listen = list(self.fodboldtur)
+            samletbeløb = list(self.fodboldtur.values())
+            x = sum(samletbeløb)
+            self.total = x
+            self.target = 4500
+            # creating tkinter window
+            self.root = Tk()
 
-    pass
+            # TEXT
+
+            velkomst = Label(self.root, text="Velkommen til fodboldtur GUI")
+            velkomst.pack(pady=10)
+
+            # Progress bar widget
+            self.progressLabelText = StringVar()
+            self.progressLabelText.set(f"Indsamlet: {self.total} af {self.target} kroner:")
+
+            self.progressLabel = Label(self.root, textvariable=self.progressLabelText)
+            self.progressLabel.pack()
+            self.progress = Progressbar(self.root, orient=HORIZONTAL,
+                                        length=250, mode='determinate')
+            self.progress['value'] = self.total / self.target * 100
+            # print(self.progress['length'])
+            # print(self.progress['value'])
+            # BUTTONS
+            self.progress.pack(padx=20)
+
+            listButton = Button(self.root, text="Liste over indbetalinger", command=lambda: listWindowClass(self))
+            listButton.pack(padx=20, pady=10, side=LEFT)
+
+            payButton = Button(self.root, text="Indbetal", command=lambda: payWindowClass(self))
+            payButton.pack(padx=20, pady=10, side=LEFT)
+
+            bottom3Button = Button(self.root, text="afslut", command=lambda: saveWindowCLass(self))
+            bottom3Button.pack(padx=20, pady=10, side=LEFT)
+
+            # infinite loop
+            mainloop()
+
+    if __name__ == '__main__':
+        main = mainWindowClass()
 
